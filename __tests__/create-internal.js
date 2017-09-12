ed2c6f0722f801b2d5557189a0fabd9e0bb96eff
Ron Waldon <jokeyrhyme@gmail.com>
2017-09-12 20:56:00 +1000

    `@jokeyrhyme/node-init`

--- __tests__/create-internal.js
+++ __tests__/create-internal.js
@@ -1,35 +1,35 @@
 /* @flow */
-'use strict'
+'use strict';
 
-const createInternal = require('../index.js')
+const createInternal = require('../index.js');
 
 test('createInternal', () => {
-  expect(typeof createInternal).toBe('function')
-})
+  expect(typeof createInternal).toBe('function');
+});
 
 test('internal = createInternal()', () => {
-  const internal = createInternal()
-  expect(typeof internal).toBe('function')
-})
+  const internal = createInternal();
+  expect(typeof internal).toBe('function');
+});
 
 test('internal(a).abc = 123', () => {
-  const internal = createInternal()
-  const a = {}
-  const b = {}
+  const internal = createInternal();
+  const a = {};
+  const b = {};
 
-  internal(a).abc = 123
+  internal(a).abc = 123;
 
-  expect(internal(a).abc).toBe(123)
-  expect(internal(b).abc).not.toBeDefined()
-})
+  expect(internal(a).abc).toBe(123);
+  expect(internal(b).abc).not.toBeDefined();
+});
 
 test('a = createInternal(); b = createInternal()', () => {
-  const a = createInternal()
-  const b = createInternal()
-  const obj = {}
+  const a = createInternal();
+  const b = createInternal();
+  const obj = {};
 
-  a(obj).abc = 123
+  a(obj).abc = 123;
 
-  expect(a(obj).abc).toBe(123)
-  expect(b(obj).abc).not.toBeDefined()
-})
+  expect(a(obj).abc).toBe(123);
+  expect(b(obj).abc).not.toBeDefined();
+});
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /* @flow */
-'use strict'
+'use strict';
 
 /* ::
 type MapObject = { [id:string]: any }
 */
 
-module.exports = function createInternal () {
-  const map /* : WeakMap<Object, MapObject> */ = new WeakMap()
-  return (object /* : Object */) /* : MapObject */ => {
-    const values = map.get(object) || {}
+module.exports = function createInternal() {
+  const map /* : WeakMap<Object, MapObject> */ = new WeakMap();
+  return function(object /* : Object */) /* : MapObject */ {
+    const values = map.get(object) || {};
     if (!map.has(object)) {
-      map.set(object, values)
+      map.set(object, values);
     }
-    return values
-  }
-}
+    return values;
+  };
+};
